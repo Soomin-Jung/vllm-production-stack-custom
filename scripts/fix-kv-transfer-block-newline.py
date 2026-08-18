8fd35f9fd896076f06b70c98e10ffb15d93fae2b
Soomin-Jung <162991051+Soomin-Jung@users.noreply.github.com>
2026-08-18 10:57:39 +0900

    chore: restore reviewed KV transfer source form

--- scripts/fix-kv-transfer-block-newline.py
+++ scripts/fix-kv-transfer-block-newline.py
@@ -4,18 +4,6 @@ path = Path(__file__).resolve().parents[1] / "helm/templates/deployment-vllm-mul
 text = path.read_text()
 old = '''          {{- if $modelSpec.lmcacheConfig }}
           {{-   if $modelSpec.lmcacheConfig.enabled }}
-          - "--kv-transfer-config"
-          {{/* kv_role 및 실패 정책은 Helm 렌더 시 동적으로 결정되므로 KV transfer config는 profile이 아닌 template에서 관리 */}}
-          - |
-            {"kv_connector": "LMCacheConnectorV1",
-             "kv_role": "{{ $kv_role }}"{{ if $modelSpec.lmcacheConfig.enable_kv_load_failure_policy }},
-             "kv_load_failure_policy": "recompute"{{ end }}
-            }
-          {{   end }}
-          {{ end }}
-'''
-new = '''          {{- if $modelSpec.lmcacheConfig }}
-          {{-   if $modelSpec.lmcacheConfig.enabled }}
           {{-     $kvTransferConfig := dict "kv_connector" "LMCacheConnectorV1" "kv_role" $kv_role }}
           {{-     if $modelSpec.lmcacheConfig.enable_kv_load_failure_policy }}
           {{-       $_ := set $kvTransferConfig "kv_load_failure_policy" "recompute" }}
@@ -26,6 +14,18 @@ new = '''          {{- if $modelSpec.lmcacheConfig }}
             {{ $kvTransferConfig | toJson }}
           {{   end }}
           {{ end }}
+'''
+new = '''          {{- if $modelSpec.lmcacheConfig }}
+          {{-   if $modelSpec.lmcacheConfig.enabled }}
+          - "--kv-transfer-config"
+          {{/* kv_role 및 실패 정책은 Helm 렌더 시 동적으로 결정되므로 KV transfer config는 profile이 아닌 template에서 관리 */}}
+          - |
+            {"kv_connector": "LMCacheConnectorV1",
+             "kv_role": "{{ $kv_role }}"{{ if $modelSpec.lmcacheConfig.enable_kv_load_failure_policy }},
+             "kv_load_failure_policy": "recompute"{{ end }}
+            }
+          {{-   end }}
+          {{- end }}
 '''
 count = text.count(old)
 if count != 1:
